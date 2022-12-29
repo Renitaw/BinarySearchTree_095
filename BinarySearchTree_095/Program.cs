e2474b4a58915ae3cc4381628e7528eb8b02dc11
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-12-29 17:05:24 +0700

    Create a Node class

--- BinarySearchTree_095/Program.cs
+++ BinarySearchTree_095/Program.cs
@@ -6,6 +6,21 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree_095
 {
+    class Node
+    {
+        public string info;
+        public Node lefftchild;
+        public Node Rightchild;
+
+        // Constructor for the node class
+
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            lefftchild = l;
+            Rightchild = r;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
