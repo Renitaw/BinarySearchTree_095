75155dae3ce54f4da76c924e2a851edef6297c2f
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-12-29 17:08:48 +0700

    Crreate BinaryTree class

--- BinarySearchTree_095/Program.cs
+++ BinarySearchTree_095/Program.cs
@@ -21,6 +21,15 @@ namespace BinarySearchTree_095
             Rightchild = r;
         }
     }
+    /* A node class consist of three things, the information, references to the right child, and references to the left child */
+    class BinaryTree
+    {
+        public Node ROOT;
+        public  BinaryTree()
+        {
+            ROOT = null; /*Initializing ROOT to null*/
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
